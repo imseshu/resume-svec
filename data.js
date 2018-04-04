0e262a739d0ee635455c06b2a211a5aac2fc7f79
imseshu <seshusharma73@gmail.com>
2018-04-04 12:34:53 +0530

    Added

--- data.js
+++ data.js
@@ -15,6 +15,7 @@ loadJSON("data.json",function(text){
   console.log(data);
   career(data.career);
   education(data.education);
+  skills(data.skills);
 })
 var right=document.querySelector(".content");
 function career(car){
@@ -30,8 +31,43 @@ function career(car){
 }
 function education(edu){
   var h3=document.createElement("h3");
-  h3.textContent="EDUCATION";
+  h3.textContent="Education";
   right.appendChild(h3);
   var hr=document.createElement("hr");
   right.appendChild(hr);
+var u1=document.createElement("u1");
+right.appendChild(u1);
+for(var i=edu.length-1;i>=0;i--){
+  console.log(edu[i].degree);
+  var li=document.createElement("li");
+  li.textContent=edu[i].degree;
+  right.appendChild(li);
+  var p=document.createElement("p");
+  p.textContent=edu[i].school;
+  li.appendChild(p);
+  var ul1=document.createElement("u1");
+  u1.appendChild(ul1);
+  for(var j=0;j<edu[i].data.length;j++){
+    var li1=document.createElement("li");
+    li1.textContent=edu[i].data[j];
+    ul1.appendChild(li1);
+  }
+
+}
+}
+function skills(skill){
+  var h3=document.createElement("h3");
+  h3.textContent="Skills";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var table=document.createElement("table");
+  table.border="1";
+  right.appendChild(table);
+  let row="";
+  for(var i=0;i<skill.length;i++)
+  {
+    row+="<tr><td>"+skill[i].name+"</td><td>"+skill[i].info+"</td></tr>";
+  }
+  table.innerHTML=row;
 }
